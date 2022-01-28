76a28c7910aebcac0baa5f9d67c4ddd3097c23bb
Ripon75 <riponahmedjrtr@gmail.com>
2022-01-28 22:59:30 +0600

    Admin panel setup

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::middleware(['auth', 'isAdmin'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('admin.index');
+    });
+});
